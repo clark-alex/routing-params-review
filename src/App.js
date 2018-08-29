9ec4f50ef4d7121cc20b343a49d007136459c78b
Alex <alex.r.clark21@gmail.com>
2018-08-28 18:49:55 -0600

    changes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
+import { HashRouter, Switch, Route } from 'react-router-dom'
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        set up routing you can either do it all here or make a routes folder, it is up to you.
-      </div>
+      <HashRouter>
+        <Switch>
+        </Switch>
+      </HashRouter>
     );
   }
 }
--- src/Components/Dashboard.js
+++ src/Components/Dashboard.js
@@ -1,12 +1,25 @@
 import React, { Component } from 'react';
 
 class Dashboard extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            charVal: '',
+            text:''
+        }
+        
+    }
+    handleInput (e) {
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+    }
 
     render() {
         return (
             <div>
                 {/* the value on each character represents their swapi id. for example https://swapi.co/api/people/1/ will return Luke and all of his info. */}
-                <select>
+                <select name='Val' onChange={this.handleInput}>
                     <option value="" selected disabled hidden>Select a character</option>
                     <option value="1">Luke</option>
                     <option value="2">C-3P0</option>
@@ -25,6 +38,7 @@ class Dashboard extends Component {
                     <option value="36">Jar Jar Binks</option>
                     <option value="44">Darth Maul</option>
                 </select>
+                <input name="text" onChange={this.handleInput}/>
                 <button>Get Details</button>
                 {/* when this button is clicked it should link to the details component. */}
             </div>
